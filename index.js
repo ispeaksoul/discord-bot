ab8a59b7d8e166b6971e061e8c809ca73b38c33a
ispeaksoul <kiawatsingg@gmail.com>
2025-05-20 07:34:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const { Client, IntentsBitField, Collection } = require('discord.js');
+require('dotenv').config();
+const fs = require('fs');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.DirectMessages,
+  ],
+  partials: ['CHANNEL', 'MESSAGE'], // Required to receive DMs
+});
+
+client.commands = new Collection();
+
+// Load commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+// Load events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.TOKEN);
